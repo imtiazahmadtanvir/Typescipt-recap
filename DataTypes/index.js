47a3e1d79c1adc11e7a4c88b64a8f053d89dbb3f
imtiazahmadtanvir <imtiaztanveer07@gmail.com>
2026-04-27 17:38:11 +0600

    using void types in the functions

--- DataTypes/index.js
+++ DataTypes/index.js
@@ -14,5 +14,10 @@ age = 25;
 isLoggedIn = true;
 //using concat method to concatenate firstName and LastName and assign it to fullName variable
 fullName = firstName.concat(" ", LastName);
-console.log(fullName.split(" ")); // split method to split fullName into an array of firstName and LastName
+console.log(fullName.split(" "));
 console.log(`User ID: ${userId}\nfull Name: ${fullName}\nLast Name: ${LastName}\nAge: ${age} \nIs Logged In: ${isLoggedIn}`);
+//void is a data type that represents the absence of a value. It is used to indicate that a function does not return a value. In this case, the display function does not return anything, so we can use void as the return type.
+function display() {
+    console.log("Hello i am displaying a message");
+}
+display();
