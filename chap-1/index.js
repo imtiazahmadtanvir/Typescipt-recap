87fc9a03a057bdca9d286a202fe1b2579184be4d
imtiazahmadtanvir <imtiaztanveer07@gmail.com>
2026-04-27 17:06:17 +0600

    update init. file

---
+++ chap-1/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// let userName:string; 
+// userName="Imtiaz Ahmad";
+// let age:number = 25;
+// console.log(userName);
+// console.log(age);
+// types must be same for both parameters, otherwise it will throw an error. In this case, n2 is a string and n1 is a number, so it will throw an error. To fix this, we can either change n2 to a number or change n1 to a string.
+function add(n1, n2) {
+    return console.log(n1 + n2);
+}
+add(5, 15);
